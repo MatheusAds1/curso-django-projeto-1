9c9007b521539005aeda28d7b4bc529b89f8cb23
Matheus Roberto <matheusrobertoads@hotmail.com>
2022-12-13 22:18:23 -0300

    tags

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Category, Recipe
+from django.contrib.contenttypes.admin import GenericStackedInline
+from tag.models import Tag
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
+
+
+class TagInline(GenericStackedInline):
+    model = Tag
+    fields = 'name',
+    extra = 1
 
 
 admin.site.register(Category, CategoryAdmin)
--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
     path('recipes/search/', views.RecipeListViewSearch.as_view(), name="search"),
+    path('recipes/tags/<slug:slug>', views.RecipeListViewTag.as_view(), name="tag"),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
     path('recipes/api/v1/', views.RecipeListViewHomeApi.as_view(), name="recipes_api_v1"),
     path('recipes/api/v1/<int:pk>/', views.RecipeDetailApi.as_view(), name="recipe_detail_api_v1"),
